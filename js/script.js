57dfbbd87e5d654982445cbc0d951fed9119d316
Mona Mohammed <mona00mona1997@gmail.com>
2023-08-13 13:10:09 +0300

    menu Done

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ const button = document.querySelector(".navbar-toggler");
 function checkButtonState() {
   if (button.classList.contains("collapsed")) {
     document.querySelector("header").classList.remove("mobile");
-    document.querySelector("#navbarScroll").classList.add("d-none");
+    document.querySelector("#navbarSupportedContent").classList.add("d-none");
   } else {
     document.querySelector("header").classList.add("mobile");
-    document.querySelector("#navbarScroll").classList.remove("d-none");
+    document.querySelector("#navbarSupportedContent").classList.remove("d-none");
   }
   setTimeout(function () {
     document
-      .querySelector("#navbarScroll")
+      .querySelector("#navbarSupportedContent")
       .classList.remove("d-none");
   }, 0);
 }
